90bb44bb06ee30ac6f44421d16c67372f1524de6
erikh2000 <erikrhermansen@Eriks-iMac-2.local>
2025-04-18 17:32:38 -0700

    Initial commit.

---
+++ src/Gallery.tsx
@@ -0,0 +1,11 @@
+import style from './Gallery.module.css'
+
+function Gallery() {
+  return (
+    <div className={style.container}>
+      <h1>Decent Portal Test App</h1>
+    </div>
+  );
+}
+
+export default Gallery;
---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import path from 'path';
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'decent-portal',
+      formats: ['es', 'umd']
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        format: 'es',
+        dir: 'dist',
+        globals: { 
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    },
+    sourcemap: true
+  }
+});
