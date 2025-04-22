f5153af663393e9bf937b0f0010cb9d403ec4ebd
erikh2000 <erikrhermansen@Eriks-iMac-2.local>
2025-04-22 08:38:47 -0700

    CSS is now injected into the JS to avoid an extra import in consuming projects.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import path from 'path';
 import react from '@vitejs/plugin-react'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'; 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cssInjectedByJsPlugin()],
   server: {
     port: 3000,
   },
@@ -22,7 +23,6 @@ export default defineConfig({
     rollupOptions: {
       external: ['react', 'react-dom'],
       output: {
-        format: 'es',
         dir: 'dist',
         globals: { 
           react: 'React',
